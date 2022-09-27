1920b78c877ebf645df9aa42c753f22d01ece6d0
Marlene92 <114458183+Marlene92@users.noreply.github.com>
2022-09-26 22:43:12 -0500

    Add files via upload
    Aplicación web en Spring Boot con reportes en PDF y excel , paginación , login y operaciones CRUD.

---
+++ mini-control-empleados/src/main/java/com/gestion/empleados/MiniControlEmpleadosApplication.java
@@ -0,0 +1,14 @@
+package com.gestion.empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniControlEmpleadosApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo 5");
+		SpringApplication.run(MiniControlEmpleadosApplication.class, args);
+	}
+
+}
